150770cdd73de775a2f7aaab8e1e87c48eea71d7
ananth168 <65211656+ananth168@users.noreply.github.com>
2020-06-27 13:44:52 +0530

    Update SelectionSortExample.java

--- SelectionSortExample/src/SelectionSortExample.java
+++ SelectionSortExample/src/SelectionSortExample.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
   
 public class SelectionSortExample 
 {  
-   public static void main(String args[])  
+   public static void main( String args[] )  
    {  
        int size, i, j, temp;  
        int arr[] = new int[50];  
-       Scanner scan = new Scanner(System.in);  
+       Scanner scan = new Scanner( System.in );  
          
-       System.out.print("Enter Array Size : ");  
+       System.out.print( "Enter Array Size : " );  
        size = scan.nextInt();  
          
        System.out.print("Enter Array Elements : ");  
-       for(i=0; i<size; i++)  
+       for( i = 0 ; i < size ; ++i )  
        {  
            arr[i] = scan.nextInt();  
        }  
          
-       System.out.println("Sorting Array using Selection Sort method");  
-       for(i=0; i<size; i++)  
+       System.out.println( "Sorting Array using Selection Sort method" );  
+       for( i = 0 ; i < size ; ++i )  
        {  
-           for(j=i+1; j<size; j++)  
+           for( j = i+1 ; j < size ; ++j )  
            {  
                if(arr[i] > arr[j])  
                {  
@@ -31,10 +31,10 @@ public class SelectionSortExample
            }  
        }  
          
-       System.out.print("Now the Array after Sorting is:");  
-       for(i=0; i<size; i++)  
+       System.out.print( "Now the Array after Sorting is:" );  
+       for( i = 0 ; i < size ; i++ )  
        {  
-           System.out.print(arr[i]+ "  ");  
+           System.out.print( arr[i]+  "  " );  
        }  
    }  
 }  
